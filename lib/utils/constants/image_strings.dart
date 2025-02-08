e06c91ebcfcd22200b02542c9fd2038e745a92c7
codingbecky <beckyjin873@gmail.com>
2025-02-08 15:35:16 -0800

    Initial commit

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+class TImages{
+  static const String onBoardingImage1 = "assets/images/1.png";
+  static const String onBoardingImage2 = "assets/images/2.png";
+  static const String onBoardingImage3 = "assets/images/3.png";
+
+  //social logos
+  static const String google = "assets/logos/google_logo.png";
+  static const String facebook = "assets/logos/facebook-logo.png";
+
+}

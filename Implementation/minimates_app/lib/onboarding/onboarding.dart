f988b903215c896edf1c942b102354b672f851bd
codingbecky <beckyjin873@gmail.com>
2025-02-11 00:42:26 -0800

    add android app in implemenatation folder

---
+++ Implementation/minimates_app/lib/onboarding/onboarding.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:untitled1/onboarding/onboarding_controller.dart';
+import 'package:untitled1/utils/constants/sizes.dart';
+
+import '../utils/constants/colors.dart';
+import '../utils/constants/image_strings.dart';
+import '../utils/constants/text_strings.dart';
+import 'onboarding_dot_navigation.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardingController());
+
+
+    return  Scaffold(
+      body: Stack(
+        children: [
+          /// horizontal scrollable pages
+          PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
+            children: [
+              onBoardingPage(title: TTexts.onBoardingTitle1, subTitle: TTexts.onBoardingSubTitle1, image: TImages.onBoardingImage1),
+              onBoardingPage(title: TTexts.onBoardingTitle2, subTitle: TTexts.onBoardingSubTitle2, image: TImages.onBoardingImage2),
+              onBoardingPage(title: TTexts.onBoardingTitle3, subTitle: TTexts.onBoardingSubTitle3, image: TImages.onBoardingImage3),
+
+            ],
+          ),
+          /// skip button
+          Positioned(
+              top: TSizes.appBarHeight,
+              right: TSizes.defaultSpace,
+              child: TextButton(onPressed: () => OnBoardingController.instance.skipPage(), child: const Text(
+                'Skip',
+                style: TextStyle(
+                fontSize: 20,          // Set the desired font size (adjust as needed)
+                color: TColors.secondary,      // Set the desired text color
+              ),)),
+          ),
+          /// dot navigation indicator
+          onBoardingDotNavigation(),
+          
+          /// circular button
+          Positioned(
+              right: TSizes.defaultSpace,
+              bottom: TSizes.spaceBottom,
+              child: ElevatedButton(
+                  onPressed: () => OnBoardingController.instance.nextPage(),
+                  style: ElevatedButton.styleFrom(shape: const CircleBorder(
+                    side: BorderSide(
+                      color: TColors.secondary, // Set the border color to accent
+                      width: 0,            // Adjust the width as needed
+                    ),
+                  ), backgroundColor: TColors.secondary),
+                  child: const Text('Next')))
+
+        ],
+
+      ),
+    );
+  }
+}
+
+
+
+class onBoardingPage extends StatelessWidget {
+  const onBoardingPage({
+    super.key, required this.title, required this.subTitle, required this.image,
+  });
+
+  final String title, subTitle, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.spaceBottom),
+      child: Column(
+        children: [
+          Image(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height * 0.4,
+            image: AssetImage(image),
+          ),
+          SizedBox(height:TSizes.spaceBtwSections),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height:TSizes.spaceBtwItems),
+          Text(
+            subTitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+
+      ),
+    );
+  }
+}
+
